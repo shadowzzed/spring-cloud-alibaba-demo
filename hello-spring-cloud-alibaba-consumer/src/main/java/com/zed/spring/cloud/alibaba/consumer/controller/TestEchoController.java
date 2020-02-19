9aedf87976171d716458f46c74ed70997a7e5e69
Zed <shadowl91@163.com>
2020-02-19 19:00:08 +0800

    use feign to replace RestTemplate

---
+++ hello-spring-cloud-alibaba-consumer/src/main/java/com/zed/spring/cloud/alibaba/consumer/controller/TestEchoController.java
@@ -0,0 +1,25 @@
+package com.zed.spring.cloud.alibaba.consumer.controller;
+
+import com.zed.spring.cloud.alibaba.consumer.service.EchoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Zed
+ * @date 2020/2/19 下午6:53
+ * @contact shadowl91@163.com
+ */
+@RestController
+public class TestEchoController {
+
+    @Autowired
+    private EchoService echoService;
+
+    @GetMapping(value = "/feign/echo/{str}")
+    public String echo(@PathVariable("str") String str) {
+        return echoService.echo(str);
+
+    }
+}
---
+++ hello-spring-cloud-alibaba-consumer/src/main/java/com/zed/spring/cloud/alibaba/consumer/service/EchoService.java
@@ -0,0 +1,24 @@
+package com.zed.spring.cloud.alibaba.consumer.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * use feign to connect to nacos and get serivce
+ * @author Zed
+ * @date 2020/2/19 下午6:54
+ * @contact shadowl91@163.com
+ */
+@FeignClient(name = "service-provider")
+public interface EchoService {
+
+    /**
+     * if provider provides pathvariable this interface must use pathvariable too
+     * or it cannot get the service
+     * @param str
+     * @return
+     */
+    @GetMapping("/echo/{str}")
+    public String echo(@PathVariable("str") String str);
+}
