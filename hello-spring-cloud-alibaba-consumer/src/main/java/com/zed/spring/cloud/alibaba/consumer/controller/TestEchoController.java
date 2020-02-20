b3826f03bc27a589da5bcd6a21355bc62b1477b9
Zed <shadowl91@163.com>
2020-02-20 17:57:06 +0800

    add dynamic nacos config to consumer

--- hello-spring-cloud-alibaba-consumer/src/main/java/com/zed/spring/cloud/alibaba/consumer/controller/TestEchoController.java
+++ hello-spring-cloud-alibaba-consumer/src/main/java/com/zed/spring/cloud/alibaba/consumer/controller/TestEchoController.java
@@ -2,6 +2,8 @@ package com.zed.spring.cloud.alibaba.consumer.controller;
 
 import com.zed.spring.cloud.alibaba.consumer.service.EchoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/2/19 下午6:53
  * @contact shadowl91@163.com
  */
+@RefreshScope
 @RestController
 public class TestEchoController {
 
     @Autowired
     private EchoService echoService;
 
+    @Value("${user.name}")
+    private String username;
+
     @GetMapping(value = "/feign/echo/{str}")
     public String echo(@PathVariable("str") String str) {
-        return echoService.echo(str);
+        return echoService.echo(str) + username;
+    }
+
+    @GetMapping(value = "/feign/echo")
+    public String echo1() {
+        return echoService.echo(username);
     }
 
     @GetMapping("/lb")
